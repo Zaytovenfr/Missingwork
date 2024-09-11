8dcac59529b137ef3081386558f217883939aa8a
Zavaon Taylor <taylozav000@warren.k12.in.us>
2024-09-11 09:00:06 -0400

    yes

---
+++ main.py
@@ -0,0 +1,7 @@
+name = input("Enter your first name:Zavaon ")
+#INSTRUCTION #1 ask the user with the input() "How many repeats do you want: "
+# Store this in a variable called repeats
+repeats = input("How many repeats do you want:1 ")
+###YOUDO convert repeats to repeats_num with the int()
+repeats_num = int(repeats)
+print(name * repeats_num)
